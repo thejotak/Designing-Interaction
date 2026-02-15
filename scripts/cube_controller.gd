79102502646dc3d960e26bc0961f0c55e9d75866
Johan <johanvdtak@proton.me>
2026-02-15 15:52:10 +0100

    changed cube controller and stats to make more responsive

--- scripts/cube_controller.gd
+++ scripts/cube_controller.gd
@@ -2,6 +2,7 @@ extends RigidBody3D
 
 @export var force := 1.5
 @export var forward_force = 1
+@export var up_force = 1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -23,10 +24,23 @@ func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, n
 
 
 func move_cube(mouse_pressed_position: Vector3):
-	print(global_position)
-	print(global_position - mouse_pressed_position  * forward_force)
-	print("  ")
-	apply_impulse((global_position - mouse_pressed_position) * force, mouse_pressed_position)
+	
+	var direction3D = global_position - mouse_pressed_position
+	var direction2D = Vector2(direction3D.x, direction3D.z)
+	direction2D = direction2D.normalized()
+	
+	direction3D.y = up_force
+	direction2D *= forward_force
+	
+	direction3D.x = direction2D.x
+	direction3D.z = direction2D.y
+	
+	apply_impulse(direction3D, mouse_pressed_position)
+	
+	#print(global_position)
+	#print(global_position - mouse_pressed_position  * forward_force)
+	#print("  ")
+	#apply_impulse((global_position - mouse_pressed_position) * force, mouse_pressed_position)
 	
 	
 	
