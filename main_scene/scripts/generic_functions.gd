4389100f716d7ed9365038e04e39b3377df5a3e6
Johan <johanvdtak@proton.me>
2026-03-14 15:04:59 +0100

    the cube now changes color and shape over time

--- main_scene/scripts/generic_functions.gd
+++ main_scene/scripts/generic_functions.gd
@@ -8,3 +8,8 @@ func find_node_in_children(parent, type):
 		if grandchild != null:
 			return grandchild
 	return null
+
+func get_sibling_by_type(parent, type):
+	for child in parent.get_children():
+		if is_instance_of(child, type):
+			return child
--- main_scene/scripts/player_dryness_behaviour.gd
+++ main_scene/scripts/player_dryness_behaviour.gd
@@ -1,13 +1,42 @@
 extends Node
 
-var t : float 
+@export var start_color : Color
+@export var end_color : Color
 
+var t : float = 0
+
+var time_to_dry_out := 20.0
+var is_drying_out = false
+
+signal change_shape(shape_change)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	start_drying_out()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if (is_drying_out):
+		t += delta
+		
+		# Get material
+		var mesh_node = GenericFunctions.get_sibling_by_type(get_parent(), MeshInstance3D)
+		var material : Material = mesh_node.get_active_material(0)
+		
+		# Change color
+		var color_difference = end_color - start_color
+		var new_color = start_color + color_difference * (t/time_to_dry_out)
+		material.albedo_color = new_color
+		
+		# Change Shape
+		change_shape.emit(delta/time_to_dry_out)
+		
+		
+		if (t >= time_to_dry_out):
+			is_drying_out = false
+	
+
+func start_drying_out():
+	t = 0
+	is_drying_out = true
