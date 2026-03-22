fc21f1a15eb8514fa821dd2cebbd6226084d1497
Johan <johanvdtak@proton.me>
2026-03-22 14:05:22 +0100

    refactored dryness_behaviour with enum

--- main_scene/scripts/player_dryness_behaviour.gd
+++ main_scene/scripts/player_dryness_behaviour.gd
@@ -14,10 +14,21 @@ extends Node
 @export var start_forward_force : float
 @export var end_forward_force : float
 
+@export var start_mass : float
+@export var end_mass : float
+
+
 var t : float = 0
 
+enum player_state 
+{
+	becoming_cube = -1,
+	stable = 0,
+	becoming_sphere = 1,
+}
+var state : player_state = player_state.stable
+
 @export var time_to_dry_out := 20.0
-var is_drying_out = false
 
 signal change_shape(shape_change)
 
@@ -34,13 +45,19 @@ func _ready() -> void:
 	material.albedo_color = start_color
 	material.uv1_scale = start_uv1_scale
 	
-	start_drying_out()
+	t = 0
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (is_drying_out):
+	if state == player_state.becoming_cube:
+		t -= delta
+	if state == player_state.becoming_sphere:
 		t += delta
+	
+	
+	if (state == player_state.becoming_cube || state == player_state.becoming_sphere):
 		
 		# Change color
 		var new_color = update_variable(start_color, end_color)
@@ -58,20 +75,38 @@ func _process(delta: float) -> void:
 		var new_forward_force = update_variable(start_forward_force, end_forward_force)
 		player.forward_force = new_forward_force
 		
+		# Change mass
+		var new_mass = update_variable(start_mass, end_mass)
+		player.mass = new_mass
+		
 		
 		# Change Shape
-		change_shape.emit(delta/time_to_dry_out)
+		change_shape.emit(delta/time_to_dry_out * state)
 		
 		
 		if (t >= time_to_dry_out):
-			is_drying_out = false
+			print("the cube is completely wet!")
+			state = player_state.stable
 	
 
-func update_variable(start_value, end_value,):
+func update_variable(start_value, end_value):
 	var difference = end_value - start_value
 	var new_value = start_value + difference * (t/time_to_dry_out)
 	return new_value
 
-func start_drying_out():
-	t = 0
-	is_drying_out = true
+
+
+func become_stable(node: Node3D):
+	if (node.is_in_group("player")):
+		state = player_state.stable
+		print("The cube is stable!")
+
+func become_round(node: Node3D):
+	if (node.is_in_group("player")):
+		state = player_state.becoming_sphere
+		print("The cube is becoming rounder!")
+
+func become_square(node: Node3D):
+	if (node.is_in_group("player")):
+		state = player_state.becoming_cube
+		print("The cube is becoming more square!") 
